bf2cedaab812777f188bf4c45c5fbd6c99469933
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2020-07-02 17:48:38 -0300

    Criação da Home do Dashboard

--- SICAT/resources/views/welcome.blade.php
+++ SICAT/resources/views/welcome.blade.php
@@ -3,22 +3,138 @@
 @section('title', 'Página inicial')
 
 @section('content_header')
-    <h1>Página inicial</h1>
+<h1>Página inicial</h1>
 @stop
 
 @section('breadcrumb')
-    <li class="breadcrumb-item"><a href="#">Home</a></li>
+<li class="breadcrumb-item"><a href="#">Home</a></li>
 @stop
 
 @section('content')
-<p>Welcome to this beautiful admin panel.</p>
+
+<div class="row">
+    <h5>Ordens de Serviço</h5>
+</div>
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>150</h3>
+
+                <p>Total</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>50</h3>
+
+                <p>Finalizado</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-check"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>50</h3>
+
+                <p>Pendente</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-flag"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>50</h3>
+
+                <p>Atrasados</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-flag"></i>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <h5>Empréstimos</h5>
+</div>
+
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>150</h3>
+
+                <p>Total</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>50</h3>
+
+                <p>Devolvidos</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-check"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>50</h3>
+
+                <p>Emprestados</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-flag"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>50</h3>
+
+                <p>Atrasados</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-flag"></i>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
 
 @section('footer')
-    <div class="float-right d-none d-sm-inline">
-        <strong>Versão</strong> 1.0
-    </div>
-    <strong>Copyright &copy; {{date("Y")}} <a href="#">Carlos A. & Victor H</a>.</strong> Todos os direitos reservados.
+<div class="float-right d-none d-sm-inline">
+    <strong>Versão</strong> 1.0
+</div>
+<strong>Copyright &copy; {{date("Y")}} <a href="#">Carlos A. & Victor H</a>.</strong> Todos os direitos reservados.
 @stop
 
 @section('css')
@@ -27,6 +143,5 @@
 
 @section('js')
 <script>
-    console.log('Hi!');
 </script>
 @stop
