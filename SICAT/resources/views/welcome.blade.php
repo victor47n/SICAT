2a984c12b7a5af5ed28f333d3ed5ce6cae32bd92
victor47n <victor47n@gmail.com>
2020-07-02 02:36:11 -0300

    Modificações em algumas coisas do layout base

--- SICAT/resources/views/welcome.blade.php
+++ SICAT/resources/views/welcome.blade.php
@@ -1,17 +1,28 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Página inicial')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Página inicial</h1>
+@stop
+
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
 @stop
 
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
 @stop
 
+@section('footer')
+    <div class="float-right d-none d-sm-inline">
+        <strong>Versão</strong> 1.0
+    </div>
+    <strong>Copyright &copy; {{date("Y")}} <a href="#">Carlos A. & Victor H</a>.</strong> Todos os direitos reservados.
+@stop
+
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+{{--    <link rel="stylesheet" href="/css/admin_custom.css">--}}
 @stop
 
 @section('js')
