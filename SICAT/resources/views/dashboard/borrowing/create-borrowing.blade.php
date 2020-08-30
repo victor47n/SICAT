073dda3914eccddc61b62e0a621c6953b7306191
victor47n <victor47n@gmail.com>
2020-08-30 02:38:36 -0300

    Começando a tabela na tela de cadastro

--- SICAT/resources/views/dashboard/borrowing/create-borrowing.blade.php
+++ SICAT/resources/views/dashboard/borrowing/create-borrowing.blade.php
@@ -55,24 +55,23 @@
                         <div id="fItems">
                             <div class="form-row align-items-end" id="item-row-0">
                                 <div class="form-group col-md-12 col-lg-4">
-                                    <label for="inputType-0">Tipo</label>
-                                    <select id="inputType-0" class="form-control" onchange="populateItemSelect(0)"
-                                            required>
-                                        <option selected disabled>Escolher...</option>
+                                    <label for="inputType">Tipo</label>
+                                    <select id="inputType" class="form-control" required>
+                                        <option selected disabled value>Escolher...</option>
                                         @foreach($types as $type)
                                             <option value="{{ $type->id }}">{{ $type->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
                                 <div class="form-group col-md-12 col-lg-4">
-                                    <label for="inputItem-0">Item</label>
-                                    <select id="inputItem-0" class="form-control" name="item_id[]" required>
+                                    <label for="inputItem">Item</label>
+                                    <select id="inputItem" class="form-control" name="item_id" required>
                                         <option selected disabled>Escolher...</option>
                                     </select>
                                 </div>
                                 <div class="form-group col-2">
                                     <label for="inputAmount">Quantidade</label>
-                                    <input type="text" class="form-control" id="inputAmount-0" name="amount[]"
+                                    <input type="text" class="form-control" id="inputAmount" name="amount"
                                            placeholder="0" required>
                                 </div>
 
@@ -85,7 +84,7 @@
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-12 col-lg-4">
-                                <label for="inputDateA">Data de aquisição</label>
+                                <label for="inputDate">Data de aquisição</label>
                                 <input type="date" class="form-control" id="inputDate" name="acquisition_date"
                                        placeholder="dd/mm/aaaa" required>
                             </div>
@@ -128,7 +127,6 @@
     <script>
         $('[data-mask]').inputmask();
 
-
         $(document).ready(function () {
 
             let a = 0;
@@ -138,36 +136,103 @@
             });
 
             $("#addItem").on('click', function () {
-                a++;
-                let html = '';
-                html += '<div class="form-row align-items-end" id="item-row-'+a+'">\n' +
-                    '                                <div class="form-group col-md-12 col-lg-4">\n' +
-                    '                                    <label for="inputType-' + a + '">Tipo</label>\n' +
-                    '                                    <select id="inputType-' + a + '" class="form-control" onchange="populateItemSelect(' + a + ')">\n' +
-                    '                                        <option selected disabled>Escolher...</option>\n' +
-                    '                                        @foreach($types as $type)\n' +
-                    '                                            <option value="{{ $type->id }}">{{ $type->name }}</option>\n' +
-                    '                                        @endforeach\n' +
-                    '                                    </select>\n' +
-                    '                                </div>\n' +
-                    '                                <div class="form-group col-md-12 col-lg-4">\n' +
-                    '                                    <label for="inputItem-' + a + '">Item</label>\n' +
-                    '                                    <select id="inputItem-' + a + '" class="form-control" name="item_id[]">\n' +
-                    '                                        <option selected disabled>Escolher...</option>\n' +
-                    '                                    </select>\n' +
-                    '                                </div>\n' +
-                    '                                <div class="form-group col-2">\n' +
-                    '                                    <label for="inputAmount">Quantidade</label>\n' +
-                    '                                    <input type="text" class="form-control" id="inputAmount-' + a + '" name="amount[]"\n' +
-                    '                                           placeholder="0">\n' +
-                    '                                </div>\n' +
-                    '\n' +
-                    '                                <div class="col-auto mb-3">\n' +
-                    '                                        <button id="delete" type="button" class="btn btn-danger"><i\n' +
-                    '                                                class="fas fa-fw fa-minus"></i></button>\n' +
-                    '                                </div>\n' +
-                    '                            </div>';
-                $('#fItems').append(html);
+                {{--a++;--}}
+                {{--let html = '';--}}
+                {{--html += '<div class="form-row align-items-end" id="item-row-'+a+'">\n' +--}}
+                {{--    '                                <div class="form-group col-md-12 col-lg-4">\n' +--}}
+                {{--    '                                    <label for="inputType-' + a + '">Tipo</label>\n' +--}}
+                {{--    '                                    <select id="inputType-' + a + '" class="form-control" onchange="populateItemSelect(' + a + ')">\n' +--}}
+                {{--    '                                        <option selected disabled>Escolher...</option>\n' +--}}
+                {{--    '                                        @foreach($types as $type)\n' +--}}
+                {{--    '                                            <option value="{{ $type->id }}">{{ $type->name }}</option>\n' +--}}
+                {{--    '                                        @endforeach\n' +--}}
+                {{--    '                                    </select>\n' +--}}
+                {{--    '                                </div>\n' +--}}
+                {{--    '                                <div class="form-group col-md-12 col-lg-4">\n' +--}}
+                {{--    '                                    <label for="inputItem-' + a + '">Item</label>\n' +--}}
+                {{--    '                                    <select id="inputItem-' + a + '" class="form-control" name="item_id[]">\n' +--}}
+                {{--    '                                        <option selected disabled>Escolher...</option>\n' +--}}
+                {{--    '                                    </select>\n' +--}}
+                {{--    '                                </div>\n' +--}}
+                {{--    '                                <div class="form-group col-2">\n' +--}}
+                {{--    '                                    <label for="inputAmount">Quantidade</label>\n' +--}}
+                {{--    '                                    <input type="text" class="form-control" id="inputAmount-' + a + '" name="amount[]"\n' +--}}
+                {{--    '                                           placeholder="0">\n' +--}}
+                {{--    '                                </div>\n' +--}}
+                {{--    '\n' +--}}
+                {{--    '                                <div class="col-auto mb-3">\n' +--}}
+                {{--    '                                        <button id="delete" type="button" class="btn btn-danger"><i\n' +--}}
+                {{--    '                                                class="fas fa-fw fa-minus"></i></button>\n' +--}}
+                {{--    '                                </div>\n' +--}}
+                {{--    '                            </div>';--}}
+                {{--$('#fItems').append(html);--}}
+
+                // let type = {
+                //                 id: $('#inputType')
+                //                     .children("option:selected")
+                //                     .val(),
+                //                 name: $('#inputType')
+                //                     .children("option:selected")
+                //                     .text()
+                //             };
+                //
+                // let item = {
+                //                 id: $('#inputItem')
+                //                     .children("option:selected")
+                //                     .val(),
+                //                 name: $('#inputItem')
+                //                     .children("option:selected")
+                //                     .text()
+                //             };
+                //
+                // let amount = $('#inputAmount').val();
+
+                let items = {
+                    type_id: $('#inputType')
+                        .children("option:selected")
+                        .val(),
+                    type_name: $('#inputType')
+                        .children("option:selected")
+                        .text(),
+                    item_id: $('#inputItem')
+                        .children("option:selected")
+                        .val(),
+                    item_name: $('#inputItem')
+                        .children("option:selected")
+                        .text(),
+                    amount: $('#inputAmount').val(),
+                }
+
+                // FAZER VERIFICAÇÃO SE O SELECT TA SEM SELECIONAR NADA
+                // console.log(items);
+                // console.log(Object.keys(items));
+                // console.log(Object.values(items));
+
+                if ($('#tItems').length === 0)
+                {
+                    let tItems = '<table id="tItems" class="table table-sm table-borderless">' +
+                                    '<thead class="thead-dark">' +
+                                        '<tr role="row">\n' +
+                                            '<th scope="col">Tipo</th>\n' +
+                                            '<th scope="col">Item</th>\n' +
+                                            '<th scope="col">Quantidade</th>\n' +
+                                            '<th scope="col">Excluir</th>\n' +
+                                        '</tr>' +
+                                    '</thead>' +
+                                    '<tbody>' +
+                                    '</tbody>';
+
+                    $('#fItems').append(tItems);
+                }
+
+                $('#tItems tbody:last-child').append(
+                    '<tr>' +
+                        '<td id="'+items.type_id+'">'+items.type_name+'</td>' +
+                        '<td id="'+items.item_id+'">'+items.item_name+'</td>' +
+                        '<td>'+items.amount+'</td>' +
+                    '</tr>'
+                );
+
             });
         });
 
@@ -204,10 +269,10 @@
             });
         });
 
-        function populateItemSelect(a) {
-            var estadoSelecionado = $('#inputType-' + a).children("option:selected").val();
-            $('#inputItem-' + a).children('option:not(:first)').remove();
-            $('#inputItem-' + a).children('option:first').prop('selected', true);
+        $("#inputType").change(function() {
+            var estadoSelecionado = $(this).children("option:selected").val();
+            $('#inputItem').children('option:not(:first)').remove();
+            $('#inputItem').children('option:first').prop('selected', true);
 
             $.ajax({
                 url: "{{ route('borrowing.select') }}",
@@ -216,7 +281,7 @@
                 context: 'json',
                 success: function (data) {
                     data.map(_data => {
-                        $('#inputItem-' + a).append($('<option>', {
+                        $('#inputItem').append($('<option>', {
                             value: _data.id,
                             text: _data.name
                         }));
@@ -226,7 +291,7 @@
                     console.log(data);
                 }
             });
-        };
+        });
     </script>
 
 @stop
